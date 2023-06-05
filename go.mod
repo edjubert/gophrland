a654484befeccf900c28cad34b753c6ad375b358
Edouard Jubert <edouard.jubert.ext@adevinta.com>
2023-06-05 18:46:07 +0200

    created simple socket handled program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gophrland
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,63 @@
+// socket-server project main.go
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	SERVER_HOST = "localhost"
+	SERVER_PORT = "9988"
+	SERVER_TYPE = "tcp"
+)
+
+func client() {
+	//establish connection
+	connection, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		panic(err)
+	}
+	///send some data
+	_, err = connection.Write([]byte("Hello Server! Greetings."))
+	buffer := make([]byte, 1024)
+	mLen, err := connection.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Println("Received: ", string(buffer[:mLen]))
+	defer connection.Close()
+}
+
+func main() {
+	fmt.Println("Server Running...")
+	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		client()
+		os.Exit(1)
+	}
+	defer server.Close()
+	fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+	fmt.Println("Waiting for client...")
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("client connected")
+		go processClient(connection)
+	}
+}
+func processClient(connection net.Conn) {
+	buffer := make([]byte, 1024)
+	mLen, err := connection.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Println("Received: ", string(buffer[:mLen]))
+	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
+	connection.Close()
+}
