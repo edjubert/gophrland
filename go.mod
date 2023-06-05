be6fd30ba82d1dbd8e127a1b052ee4378ab301cc
Edouard Jubert <edouard.jubert.ext@adevinta.com>
2023-06-05 23:15:09 +0200

    added yaml config parser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gophrland
 
 go 1.19
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v3"
 	"net"
 	"os"
 )
@@ -27,20 +28,65 @@ func client() {
 		fmt.Println("Error reading:", err.Error())
 	}
 	fmt.Println("Received: ", string(buffer[:mLen]))
-	defer connection.Close()
+	if err := connection.Close(); err != nil {
+		panic(err)
+	}
 }
 
+type Scratchpad struct {
+	Command   string `yaml:"command"`
+	Animation string `yaml:"animation"`
+	Unfocus   string `yaml:"unfocus"`
+}
+
+type Options struct {
+	Scratchpads []map[string]Scratchpad `yaml:"scratchpads"`
+}
+
+type Config struct {
+	Plugins []string `yaml:"plugins"`
+	Options Options  `yaml:"options"`
+}
+
+func readConfig(file string) {
+	dat, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Printf("Could not read file '%s' -> %v\n", file, err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(dat, &config); err != nil {
+		fmt.Printf("Could not unmarshal %v\n", err)
+	}
+
+	fmt.Println(config.Plugins)
+	fmt.Println(config.Options)
+}
+
+func closeServer(server net.Listener) {
+	if err := server.Close(); err != nil {
+		panic(err)
+	}
+}
+
+const DEFAULT_CONFIG = "./config.yaml"
+
 func main() {
+	readConfig(DEFAULT_CONFIG)
+
 	fmt.Println("Server Running...")
+
 	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		client()
 		os.Exit(1)
 	}
-	defer server.Close()
+	defer closeServer(server)
+
 	fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
 	fmt.Println("Waiting for client...")
+
 	for {
 		connection, err := server.Accept()
 		if err != nil {
@@ -55,9 +101,15 @@ func processClient(connection net.Conn) {
 	buffer := make([]byte, 1024)
 	mLen, err := connection.Read(buffer)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		panic(err)
 	}
+
 	fmt.Println("Received: ", string(buffer[:mLen]))
-	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
-	connection.Close()
+	if _, err := connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen]))); err != nil {
+		panic(err)
+	}
+
+	if err := connection.Close(); err != nil {
+		panic(err)
+	}
 }
